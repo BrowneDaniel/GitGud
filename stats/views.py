e377c6a6b7df577fbedf450e228893769ba0200f
BrowneDaniel <dbrowne2@tcd.ie>
2018-11-27 14:35:10 +0000

    django project basics set up

---
+++ stats/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def home(request):
+#   return HttpResponse("Alright cunt this the homepage")
+    return render(request, 'home.html')
